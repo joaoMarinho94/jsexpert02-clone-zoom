71ba82190c7dd4f92ef0734da87771a176e35a64
joaoMarinho94 <joaopaulim94@gmail.com>
2021-01-28 12:57:28 -0300

    Added new structure of folders

---
+++ aula 02/public/pages/room/src/business.js
@@ -0,0 +1,47 @@
+class Business {
+  constructor({ room, media, view, socketBuilder }) {
+    this.room = room
+    this.media = media
+    this.view = view
+
+    this.socketBuilder = socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .setOnUserDisconnected(this.onUserDisconnected())
+      .build()
+    this.socketBuilder.emit('join-room', this.room, 'teste01')
+
+    this.currentStream = {}
+  }
+
+  static initialize(deps) {
+    const instace = new Business(deps)
+    return instace._init()
+  }
+
+  // metodo privado
+  async _init() {
+    this.currentStream = await this.media.getCamera()
+    this.addVideoStream('test01')
+  }
+
+  addVideoStream(userId, stream = this.currentStream) {
+    const isCurrentId = false
+    this.view.renderVideo({
+      userId,
+      stream,
+      isCurrentId
+    })
+  }
+
+  onUserConnected = function () {
+    return userId => {
+      console.log('user connected!', userId);
+    }
+  }
+
+  onUserDisconnected = function () {
+    return userId => {
+      console.log('user disconnected!', userId);
+    }
+  }
+}
