42c000dadc96890a942871f80ddc279d399a4c4a
joaoMarinho94 <joaopaulim94@gmail.com>
2021-01-28 16:55:56 -0300

    Added peer server and events custom

--- aula 02/public/pages/room/src/view.js
+++ aula 02/public/pages/room/src/view.js
@@ -22,8 +22,12 @@ class View {
     return video
   }
 
-  renderVideo({ userId, stream = null, url = null, isCurrentId = false }) {
-    const video = this.createVideoElement({ src: url, srcObject: stream })
+  renderVideo({ userId, stream = null, url = null, isCurrentId = false, muted = true }) {
+    const video = this.createVideoElement({
+      muted,
+      src: url,
+      srcObject: stream
+    })
     this.appendToHtmlTree(userId, video, isCurrentId)
   }
 
@@ -39,5 +43,11 @@ class View {
 
     const videoGrid = document.getElementById('video-grid')
     videoGrid.append(div)
+  }
+
+  setParticipants(count){
+    const myself = 1
+    const participants = document.getElementById('participants')
+    participants.innerHTML = (count + myself)
   }
 }
