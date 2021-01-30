24f91e21b6c24a9ccab0b9660a4981c212c861b5
joaoMarinho94 <joaopaulim94@gmail.com>
2021-01-30 01:18:08 -0300

    Finished class 04

---
+++ aula 04/public/pages/room/src/business.js
@@ -0,0 +1,170 @@
+class Business {
+  constructor({ room, media, view, socketBuilder, peerBuilder }) {
+    this.room = room
+    this.media = media
+    this.view = view
+
+    this.socketBuilder = socketBuilder
+    this.peerBuilder = peerBuilder
+
+    this.socket = {}
+    this.currentStream = {}
+    this.currentPeer = {}
+
+    this.peers = new Map()
+    this.usersRecordings = new Map()
+  }
+
+  static initialize(deps) {
+    const instace = new Business(deps)
+    return instace._init()
+  }
+
+  // metodo privado
+  async _init() {
+    this.view.configureRecordingButton(this.onRecordPressed.bind(this))
+    this.view.configureLeaveButton(this.onLeavePressed.bind(this))
+
+    this.currentStream = await this.media.getCamera()
+    this.socket = this.socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .setOnUserDisconnected(this.onUserDisconnected())
+      .build()
+
+    this.currentPeer = await this.peerBuilder
+      .setOnError(this.onPeerError())
+      .setOnConnectionOpened(this.onPeerConnectionOpened())
+      .setOnCallReceived(this.onPeerCallReceived())
+      .setOnPeerStreamReceived(this.onPeerStreamReceived())
+      .setOnCallError(this.onPeerCallError())
+      .setOnCallClose(this.onPeerCallClose())
+      .build()
+
+    this.addVideoStream(this.currentPeer.id)
+  }
+
+  addVideoStream(userId, stream = this.currentStream) {
+    const recorderInstance = new Recorder(userId, stream)
+    this.usersRecordings.set(recorderInstance.filename, recorderInstance)
+    if (this.recordingEnabled) {
+      recorderInstance.startRecording()
+    }
+
+    const isCurrentId = userId === this.currentPeer.id
+    this.view.renderVideo({
+      userId,
+      stream,
+      isCurrentId
+    })
+  }
+
+  onUserConnected() {
+    return userId => {
+      this.currentPeer.call(userId, this.currentStream)
+    }
+  }
+
+  onUserDisconnected() {
+    return userId => {
+      console.log('user disconnected!', userId);
+
+      if (this.peers.has(userId)) {
+        this.peers.get(userId).call.close()
+        this.peers.delete(userId)
+      }
+      this.view.setParticipants(this.peers.size)
+      this.stopRecording(userId)
+      this.view.removedVideoElement(userId)
+    }
+  }
+
+  onPeerError() {
+    return error => {
+      console.error('error on peer!', error)
+    }
+  }
+
+  onPeerConnectionOpened() {
+    return (peer) => {
+      const id = peer.id
+      console.log('peer!!', peer)
+      this.socket.emit('join-room', this.room, id)
+    }
+  }
+
+  onPeerCallReceived() {
+    return call => {
+      console.log('answering call', call)
+      call.answer(this.currentStream)
+    }
+  }
+
+  onPeerStreamReceived() {
+    return (call, stream) => {
+      const callerId = call.peer
+
+      if(this.peers.has(callerId)){
+        console.log('calling twice, igoring second call...', callerId);
+        return
+      }
+
+      this.addVideoStream(callerId, stream)
+      this.peers.set(callerId, { call })
+      this.view.setParticipants(this.peers.size)
+    }
+  }
+
+  onPeerCallError() {
+    return (call, error) => {
+      console.log('an call error ocurred!', error);
+      this.view.removeVideoElement(call.peer)
+    }
+  }
+
+  onPeerCallClose() {
+    return (call) => {
+      console.log('call closed!', call.peer);
+    }
+  }
+
+  onRecordPressed(recordingEnabled) {
+    this.recordingEnabled = recordingEnabled
+    console.log('pressionou!', recordingEnabled);
+    for (const [key, value] of this.usersRecordings) {
+      if (this.recordingEnabled) {
+        value.startRecording()
+        continue;
+      }
+      this.stopRecording(key)
+    }
+  }
+
+  // se o usuario entrar e sair da call durante uma gravação
+  // precisamos parar as gravalçoes anteriores dele
+  async stopRecording(userId){
+    const usersRecordings = this.usersRecordings
+    for (const [key, value] of usersRecordings){
+      const isContexUser = key.includes(userId)
+      if(!isContexUser) continue;
+
+      const rec = value
+      const isRecordingActive = rec.recordingActive
+      if(!isRecordingActive) continue;
+
+      await rec.stopRecording()
+      this.playRecordings(key)
+    }
+  }
+
+  playRecordings(userId){
+    const user = this.usersRecordings.get(userId)
+    const videoURLs = user.getAllVideoURLs()
+    videoURLs.map(url => {
+      this.view.renderVideo({url, userId})
+    })
+  }
+
+  onLeavePressed(){
+    this.usersRecordings.forEach((value, key ) => value.download())
+  }
+}
---
+++ aula 04/public/pages/room/src/view.js
@@ -0,0 +1,88 @@
+class View {
+  constructor() {
+    this.recorderBtn = document.getElementById('record')
+    this.leaveBtn = document.getElementById('leave')
+  }
+
+  createVideoElement({ muted = true, src, srcObject }) {
+    const video = document.createElement('video')
+    video.muted = muted
+    video.src = src
+    video.srcObject = srcObject
+
+    if (src) {
+      video.controls = true
+      video.loop = true
+      Util.sleep(200).then(_ => video.play())
+    }
+
+    if (srcObject) {
+      video.addEventListener("loadedmetadata", _ => video.play())
+    }
+
+    return video
+  }
+
+  renderVideo({ userId, stream = null, url = null, isCurrentId = false }) {
+    const video = this.createVideoElement({
+      muted: isCurrentId,
+      src: url,
+      srcObject: stream
+    })
+    this.appendToHtmlTree(userId, video, isCurrentId)
+  }
+
+  appendToHtmlTree(userId, video, isCurrentId) {
+    const div = document.createElement('div')
+    div.id = userId
+    div.classList.add('wrapper')
+    div.append(video)
+
+    const div2 = document.createElement('div')
+    div2.innerText = isCurrentId ? '' : userId
+    div.append(div2)
+
+    const videoGrid = document.getElementById('video-grid')
+    videoGrid.append(div)
+  }
+
+  setParticipants(count) {
+    const myself = 1
+    const participants = document.getElementById('participants')
+    participants.innerHTML = (count + myself)
+  }
+
+  removedVideoElement(id) {
+    const element = document.getElementById(id)
+    element.remove()
+  }
+
+  toogleRecordingButtonColor(isActive = true){
+    this.recorderBtn.style.color = isActive ? 'red' : 'white'
+  }
+
+  onLeaveClick(command) {
+    return async () => {
+      command()
+      await Util.sleep(5000)
+      window.location = '/pages/home'
+    }
+  }
+
+  onRecordClick(command) {
+    this.recordingEnabled = false
+    return () => {
+      const isActive = this.recordingEnabled = !this.recordingEnabled
+      command(this.recordingEnabled)
+      this.toogleRecordingButtonColor(isActive)
+    }
+  }
+
+  configureRecordingButton(command) {
+    this.recorderBtn.addEventListener('click', this.onRecordClick(command))
+  }
+
+  configureLeaveButton(command){
+    this.leaveBtn.addEventListener('click', this.onLeaveClick(command))
+  }
+}
